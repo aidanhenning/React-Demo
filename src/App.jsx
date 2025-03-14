32eb1dd0a795e12199b19006e1662ca51553172e
aidanhenning <henningaidan@gmail.com>
2025-03-14 13:07:07 -0700

    version 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Chase from '../components/chase'
+
+const dogs = [
+  { id: 1, name: "Logan", breed: "Italian Greyhound", age: 14 },
+  { id: 2, name: "Chase", breed: "Italian Greyhound", age: 12 },
+  { id: 3, name: "Lincoln", breed: "Mixed Rescue", age: 6 },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [bestDog, setBestDog] = useState(null);
+  console.log(bestDog);
 
   return (
     <>
       <h1>Vite + React</h1>
+      <Chase></Chase>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+
+        {bestDog && <h3>{bestDog} is awesome</h3>}
+
+        {dogs.map((dog) => {
+          return <p key={dog.id} onClick={() => setBestDog(dog.name)}>{dog.name} is {dog.age} years old</p>
+        })}
       </div>
     </>
   )
